fce8221200281a08fc8b9a61012a75bd243d3b45
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-03-20 15:49:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let table=document.getElementById("sampleTable")
+let i=1;
+let j=2;
 function insert_Row() {
     //Write your code herev
-	var newrow=table.insertRow(1);
+	var newrow=table.insertRow(0);
 	var cell1=newrow.insertCell(0);
 	var cell2=newrow.insertCell(1);
 	
-  cell1.innerHtml='New Cell1'
-  cell2.innerHtml='New Cell2'
-	
+  cell1.innerHTML=`New Cell${i}`
+  cell2.innerHTML=`New Cell${j}`
+	i++;
+	j++;
   
 }
